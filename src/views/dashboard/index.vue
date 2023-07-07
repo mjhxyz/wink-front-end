89c8d643ecc41e6374028272e896c9a6277f7673
Mao <mjhxyz@foxmail.com>
2023-07-07 10:43:51 +0800

    update: show name at dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-panel">
+      <div class="dashboard-name">
+        {{ name }}
+      </div>
+      {{ role }}, 欢迎登录
+    </div>
+    <div class="dashboard-text"></div>
   </div>
 </template>
 
@@ -11,7 +17,8 @@ export default {
   name: 'Dashboard',
   computed: {
     ...mapGetters([
-      'name'
+      'name',
+      'role'
     ])
   }
 }
@@ -19,9 +26,27 @@ export default {
 
 <style lang="scss" scoped>
 .dashboard {
+  &-panel {
+    width: 100%;
+    height: 135px;
+    background: #fff;
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &-name {
+    font-size: 30px;
+    font-weight: 600;
+    margin-right: 10px;
+    color: #2d8cf0;
+  }
+
   &-container {
     margin: 30px;
   }
+
   &-text {
     font-size: 30px;
     line-height: 46px;
