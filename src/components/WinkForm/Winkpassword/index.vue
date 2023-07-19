7a3b1d2ce51e00149b99e7933c5ccb4599dff0e1
Mao <mjhxyz@foxmail.com>
2023-07-19 18:18:26 +0800

    update: format file

--- src/components/WinkForm/Winkpassword/index.vue
+++ src/components/WinkForm/Winkpassword/index.vue
@@ -1,29 +1,29 @@
 <!-- 密码框 -->
 <template>
-    <el-input class="wink-form-item" v-model="val" :placeholder="placeholder" @change="change" show-password />
+  <el-input v-model="val" class="wink-form-item" :placeholder="placeholder" show-password @change="change" />
 </template>
 
 <script>
 export default {
-    props: {
-        value: { // 绑定值
-            type: String | Number,
-            default: ''
-        },
-        placeholder: { // 提示
-            type: String,
-            default: ''
-        },
+  props: {
+    value: { // 绑定值
+      type: [String, Number],
+      default: ''
     },
-    data() {
-        return {
-            val: this.value // 付给初始值
-        }
-    },
-    methods: {
-        change(value) {
-            this.$emit('input', value)
-        }
+    placeholder: { // 提示
+      type: String,
+      default: ''
     }
+  },
+  data() {
+    return {
+      val: this.value // 付给初始值
+    }
+  },
+  methods: {
+    change(value) {
+      this.$emit('input', value)
+    }
+  }
 }
 </script>
